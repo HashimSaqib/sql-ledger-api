e3c0ec5f871fb22b7faa1f0b19030c02009fe513
HashimSaqib <hashim1saqib@gmail.com>
2026-02-24 12:18:04 +0000

    email messages

--- SL/CT.pm
+++ SL/CT.pm
@@ -550,6 +550,24 @@ sub save {
 
   $form->save_reference($dbh, $form->{db});
 
+  # save invoice_send message for this customer/vendor if message was passed
+  if ( exists $form->{message} ) {
+    my $msg_content = defined $form->{message} ? $form->{message} : '';
+    my $trans_id   = $form->{id};
+    my $existing   = $dbh->selectrow_array(
+      qq|SELECT id FROM messages WHERE trans_id = ? AND message_type = 'invoice_send'|,
+      undef, $trans_id
+    );
+    my $language_code = $form->{language_code} // 'en';
+    if ($existing) {
+      $query = qq|UPDATE messages SET content = ?, language_code = ? WHERE trans_id = ? AND message_type = 'invoice_send'|;
+      $dbh->do( $query, undef, $msg_content, $language_code, $trans_id ) || $form->dberror($query);
+    } else {
+      $query = qq|INSERT INTO messages (message_type, language_code, content, trans_id) VALUES ('invoice_send', ?, ?, ?)|;
+      $dbh->do( $query, undef, $language_code, $msg_content, $trans_id ) || $form->dberror($query);
+    }
+  }
+
   my $rc = $dbh->commit;
   
   $dbh->disconnect if $disconnect;
